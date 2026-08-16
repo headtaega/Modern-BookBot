8ded3f8d4318d2f30adc37de5563947ea972eecf
headtaega <stitch-zoom-creme@duck.com>
2026-08-16 14:35:49 +0300

    Add functions for word and character counting

---
+++ stats.py
@@ -0,0 +1,23 @@
+def get_word_count(file: str) -> str:
+    words = file.split()
+    return(f"Found {len(words)} total words")
+
+def get_character_counts(text: str) -> dict[str, int]:
+    my_dict = {}
+    low_txt = text.lower()
+    for char in low_txt:
+        if char in my_dict:
+            my_dict[char] += 1
+        else:
+            my_dict[char] = 1
+    return my_dict
+
+def sort_on(text: tuple[str, int]) -> int:
+    return text[1]
+
+def chars_dict_to_sorted_list(my_dict: dict[str, int]) -> list[tuple[str, int]]:
+    my_list = []
+    for key in my_dict:
+        my_list.append((key, my_dict[key]))
+    sorted_list = sorted(my_list, reverse=True, key=sort_on)
+    return sorted_list
